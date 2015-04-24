390001bdf9859afdb98be8e745926227c2041c97
Andrea Lekkas <andrea.lekkas@gmail.com>
2015-04-24 17:39:14 +0200

    Definition of class CellaPartenza

--- src/main/java/it/escape/server/model/game/gamemap/CellaPartenza.java
+++ src/main/java/it/escape/server/model/game/gamemap/CellaPartenza.java
@@ -2,37 +2,49 @@ package it.escape.server.model.game.gamemap;
 
 import it.escape.server.model.game.character.Action;
 import it.escape.server.model.game.gamemap.positioning.PositionCubic;
-import java.util.List;
+import java.util.ArrayList;
 
+/**Questa classe definisce le Caselle di Partenza, una per tipo (Umani, Alieni)
+ * Le caselle di partenza sono implementate come dei singleton: 
+ * Se una è già presente, non ne viene creata un'altra.
+ * @author andrea
+ */
 public class CellaPartenza extends Cella {
-	
-	private static ArrayList<CellaPartenza> cellePartenza = new ArrayList<>;
-	
-	SpawnType tipo;
-
-	private CellaPartenza(PositionCubic position, SpawnType tipo) {
-		super(position);
-		this.tipo = tipo;
-	}
-	
-	public static CellaPartenza getPartenza(SpawnType categoria) {
-		for (CellaPartenza partenza : cellePartenza)
-			if (partenza!=null && partenza.tipo==SpawnType.categoria)
-				return partenza;	
-		cellePartenza[a] = 	
-	
-	public static getCellaPartenza() {
 		
-	}
+		private static ArrayList<CellaPartenza> cellePartenza = new ArrayList<CellaPartenza>();
+		
+		SpawnType tipo;
 
-	@Override
-	public void eseguiAzione(Action esecutore) {
-		esecutore.noAction();
-	}
+		private CellaPartenza(PositionCubic position, SpawnType tipo) {
+			super(position);
+			this.tipo = tipo;
+		}
+		
+		
+		public CellaPartenza getPartenza(PositionCubic position, SpawnType tipo) {
+			
+			CellaPartenza nuovaPartenza;
+			
+			for (CellaPartenza partenza : cellePartenza)
+				if (partenza!=null)
+					if (partenza.tipo==tipo)
+						return partenza;	
+			
+			 nuovaPartenza= new CellaPartenza(position, tipo);
+			 cellePartenza.add(nuovaPartenza);
+			 return nuovaPartenza;
+		}
 
-	@Override
-	public boolean isWalkable() {
-		return false;
+		
+			@Override
+		public void eseguiAzione(Action esecutore) {
+			esecutore.noAction();
+		}
+
+		@Override
+		public boolean isWalkable() {
+			return false;
 	}
+	
 
 }
