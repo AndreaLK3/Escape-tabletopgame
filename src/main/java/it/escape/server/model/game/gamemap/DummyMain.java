d061ebbe087f4750107a48bea8f1c5f9445cd9aa
Andrea Lekkas <andrea.lekkas@gmail.com>
2015-04-24 15:33:06 +0200

    Added documentation

--- src/main/java/it/escape/server/model/game/gamemap/DummyMain.java
+++ src/main/java/it/escape/server/model/game/gamemap/DummyMain.java
@@ -5,12 +5,18 @@ import it.escape.server.model.game.gamemap.exceptions.BadCoordinatesException;
 public class DummyMain {
 
 	public static void main(String[] args) {
-		try {
-			PositionCubic coso = new PositionCubic("A0");
-			System.out.println(coso.toString());
-		} catch (BadCoordinatesException e) {
-			System.out.println("bad coord!");
+		
+		String proveAlfaNum[] = {"A0", "A1", "A2", "B0", "B1", "B2"};
+				
+		for (String var1 : proveAlfaNum)
+		{
+			try {
+				PositionCubic coso = CoordinatesConverter.fromAlphaNumToCubic(var1);
+				System.out.println(coso.toString());
+			} catch (BadCoordinatesException e) {
+				System.out.println("The coordinates given are not acceptable!");
+			}
 		}
-	}
 
+	}
 }
