6f7f4f89d6bc63f937c2c72f9384596926eda1a0
Michele <michele.mm1234@gmail.com>
2015-05-13 15:56:44 +0200

    added untested static function saveToFile

--- src/main/java/it/escape/utils/FilesHelper.java
+++ src/main/java/it/escape/utils/FilesHelper.java
@@ -1,9 +1,11 @@
 package it.escape.utils;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 public class FilesHelper {
 	
@@ -21,5 +23,11 @@ public class FilesHelper {
 	
 	public static InputStream getResourceFile(String filename) {
 		return Thread.currentThread().getContextClassLoader().getResourceAsStream(filename);
+	}
+	
+	public static void saveToFile(String filename, String data) throws FileNotFoundException {
+		PrintWriter out = new PrintWriter(filename);
+		out.print(data);
+		out.close();
 	}
 }
