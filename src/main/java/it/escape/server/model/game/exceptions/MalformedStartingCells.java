5e4c7c6dd709db8784a76fc5de536bd03206e043
Michele <michele.mm1234@gmail.com>
2015-05-19 14:55:44 +0200

    working on gamemap

---
+++ src/main/java/it/escape/server/model/game/exceptions/MalformedStartingCells.java
@@ -0,0 +1,33 @@
+package it.escape.server.model.game.exceptions;
+
+public class MalformedStartingCells extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public MalformedStartingCells() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public MalformedStartingCells(String arg0, Throwable arg1, boolean arg2,
+			boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MalformedStartingCells(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MalformedStartingCells(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MalformedStartingCells(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
