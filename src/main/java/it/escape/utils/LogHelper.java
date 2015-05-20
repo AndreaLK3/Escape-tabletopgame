578ebf03897e245c9210a712e1ad0494210cb698
Michele <michele.mm1234@gmail.com>
2015-05-20 13:16:13 +0200

    Improved logHelper, added message strings

--- src/main/java/it/escape/utils/LogHelper.java
+++ src/main/java/it/escape/utils/LogHelper.java
@@ -14,6 +14,7 @@ public class LogHelper {
 	private static Formatter simpleFormatter = new SimpleFormatter();
 	
 	public static void setDefaultOptions(Logger logger) {
+		logger.setUseParentHandlers(false);
 		logger.addHandler(consoleOutput);  // log all messages to standard error
 		logger.setLevel(Level.ALL);  // write every message to log
 		consoleOutput.setLevel(Level.ALL);  // write every log message to console
