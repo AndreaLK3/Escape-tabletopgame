824a5fd0876350cdf855237cedd0ed6ae9ba0e61
Michele <michele.mm1234@gmail.com>
2015-05-20 13:17:52 +0200

    Created ObjectCardAction interface

---
+++ src/main/java/it/escape/server/model/game/actions/cardActions/ObjectCardAction.java
@@ -0,0 +1,9 @@
+package it.escape.server.model.game.actions.cardActions;
+
+import it.escape.server.model.game.players.Player;
+
+public interface ObjectCardAction {
+	
+	public void execute(Player currentPlayer);
+	
+}
