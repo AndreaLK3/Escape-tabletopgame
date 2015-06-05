e4280a64fb0b09d52fb0ff2f7efa73437ef9dc31
Andrea Lekkas <andrea.lekkas@gmail.com>
2015-06-05 19:25:08 +0200

    working on the Swing client view

--- src/main/java/it/escape/client/Graphics/Displayer.java
+++ src/main/java/it/escape/client/Graphics/Displayer.java
@@ -7,12 +7,15 @@ import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.swing.Box;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
@@ -29,8 +32,15 @@ public class Displayer extends JFrame
 	private JLabel label2;
 	private JLabel label3;
 	private JLabel label4;
+	private JLabel label5;
+	private JLabel label6;
+	private JLabel label7;
+	private JLabel label8;
+	private JLabel label9;
 	JScrollPane mapScrollPane;
-	private JTextArea area1;
+	private JTextArea playerArea;
+	private JTextArea statusArea;
+	private JTextArea lastNoiseArea;
 	private Icon map;
    	
    	public Displayer(String string) {
@@ -39,25 +49,43 @@ public class Displayer extends JFrame
    		constraints = new GridBagConstraints();
    		
    		initializeLabels();
-   		//initializeTextAreas();
+   		initializePanels();
    		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
    		setSize(800, 600);
 		setVisible(true);
    	}
    	
    	
-	private void initializeTextAreas() {
-		area1 = new JTextArea("Giocatore1\n");
-		area1.setEditable(false);
+	private void initializePanels() {
+		JPanel panel1 = new JPanel();
+		
+		panel1.setLayout(new FlowLayout());
+		
+		playerArea = new JTextArea();
+		playerArea.setEditable(false);
+		playerArea.setText("Giocatore 1\n");
+		playerArea.setVisible(true);
+		panel1.add(playerArea);
+		
+		statusArea = new JTextArea();
+		statusArea.setEditable(false);
+		statusArea.setText("Alive\n");
+		panel1.add(statusArea);
+		
+		lastNoiseArea = new JTextArea();
+		lastNoiseArea.setEditable(false);
+		lastNoiseArea.setText("Alive\n");
+		panel1.add(lastNoiseArea);
+		
 		constraints.fill = GridBagConstraints.BOTH;
 		constraints.gridx = 0;
-		constraints.gridy = 0;
-		add(area1, constraints);
+		constraints.gridy = 2;
+		add(panel1,constraints);
+	    resetConstraints(constraints);
 	}
 
 
 	private void initializeLabels() {
-	
 		
 	label1 = new JLabel("Escape from the Aliens in Outer Space");
 	label1.setBackground(new Color(248, 213, 131));
@@ -79,38 +107,52 @@ public class Displayer extends JFrame
 	add(label2,constraints);
 	resetConstraints(constraints);
 	
-	label3 = new JLabel();
+	label3 = new JLabel("Status");
+	label3.setBackground(new Color(200, 100, 20));
+	constraints.fill = GridBagConstraints.HORIZONTAL;
+	constraints.gridx = 1;
+	constraints.gridy = 1;
+	constraints.weightx = 0;
+	constraints.weighty = 0;
+	add(label3,constraints);
+	resetConstraints(constraints);
+	
+	label4 = new JLabel("LastNoise");
+	label4.setBackground(new Color(50, 100, 200));
+	constraints.fill = GridBagConstraints.HORIZONTAL;
+	constraints.gridx = 2;
+	constraints.gridy = 1;
+	constraints.weightx = 0;
+	constraints.weighty = 0;
+	add(label4,constraints);
+	resetConstraints(constraints);
+	
+	label5 = new JLabel();
 	Icon map = new ImageIcon(ImageScaler.resizeImage("resources/galilei.jpg", 1000, 1000));
-	label3.setIcon(map);
-	mapScrollPane = new JScrollPane(label3);
+	label5.setIcon(map);
+	mapScrollPane = new JScrollPane(label5);
 	mapScrollPane.setPreferredSize(new Dimension(400,400)); 
 	constraints.fill = GridBagConstraints.BOTH;
-	constraints.gridx = 1;
+	constraints.gridx = 3;
 	constraints.gridy = 1;
 	constraints.weightx = 1;
 	constraints.weighty = 1;
+	constraints.gridwidth = 1;
+	constraints.gridheight = 2;
 	constraints.anchor = GridBagConstraints.CENTER;
 	add(mapScrollPane, constraints);
 	resetConstraints(constraints);
 	
-	label4 = new JLabel("Status");
-	label4.setBackground(new Color(90, 100, 200));
-	constraints.fill = GridBagConstraints.HORIZONTAL;
-	constraints.gridx = 0;
-	constraints.gridy = 1;
-	add(label4,constraints);
-	resetConstraints(constraints);
-   
-    label1.setOpaque(true);
-    label2.setOpaque(true);
-    label3.setOpaque(true);
-    label4.setOpaque(true);
+	List<JLabel> labels = Arrays.asList(label1,label2,label3,label4,label5);
+	
+   for (JLabel l : labels)
+	   l.setOpaque(true);
  
    
 
 	}
    	
-	/**Add a label 
+	/**Add a label  on a row... for now, unused (I could use Panels)
 	 * @param label
 	 * @param row
 	 */
