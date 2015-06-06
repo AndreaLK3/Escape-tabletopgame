16a7e615c62655601f72e4938b0cd6527272791f
Andrea Lekkas <andrea.lekkas@gmail.com>
2015-06-06 19:25:32 +0200

    ...

--- src/main/java/it/escape/client/Graphics/ObjectCardsPanel.java
+++ src/main/java/it/escape/client/Graphics/ObjectCardsPanel.java
@@ -3,6 +3,8 @@ package it.escape.client.Graphics;
 import it.escape.utils.FilesHelper;
 
 import java.awt.Image;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.List;
 
 import javax.swing.ButtonGroup;
@@ -13,12 +15,22 @@ import javax.swing.JTextField;
 
 public class ObjectCardsPanel extends JPanel {
 
+
 	private JTextField title;
 	private List<JRadioButton> objectCardsButtons;
-	private ButtonGroup objectButtonsGroup;
+	private ButtonGroup group;
 	
-	public ObjectCardsPanel () {
+	
+	/** The constructor: when it is called, the ObjectCardsPanel is updated, 
+	 * acquiring the cards in the User's possession and adding them as buttons.
+	 * @param List<String> cards */
+	public ObjectCardsPanel (List<String> cards) {
+		for (String cardName : cards) {
+			addCardButton(cardName);
+		}
 		
+		for (JRadioButton b : objectCardsButtons)
+			group.add(b);
 	}
 	
 	
@@ -27,13 +39,21 @@ public class ObjectCardsPanel extends JPanel {
 	 * has already been done by either the Connection or PlayerState.
 	 * @param cardName
 	 */
-	private void addCard(String cardName) {
+	private void addCardButton(String cardName) {
 		JRadioButton cardButton  = new JRadioButton(cardName);
 		cardButton.setIcon(new ImageIcon(getImage(cardName)));
 		objectCardsButtons.add(cardButton);
+		group.add(cardButton);
+		//cardButton.addItemListener(handler);
 		
 	}
 	
+	
+	/** If the cardName corresponds with one of the known ones, 
+	 * this method returns the corresponding image (usually a .png).
+	 * @param String cardName
+	 * @return Image (es.Attack.png)
+	 */
 	private Image getImage(String cardName) {
 		if (cardName.startsWith("attack")) {
 			return ImageScaler.resizeImage("resources/artwork/ObjectCards/Attack.png", 60, 60);
@@ -58,7 +78,14 @@ public class ObjectCardsPanel extends JPanel {
 	}
 	
 	
-	
+	/*private class RadioButtonsHandler implements ItemListener {
+
+		@Override
+		public void itemStateChanged(ItemEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}*/
+
 	
 }
 
