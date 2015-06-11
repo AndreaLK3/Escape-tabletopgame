883e7329c933c97e8ab6ba8c400e544a20580157
Michele <michele.mm1234@gmail.com>
2015-06-11 23:54:43 +0200

    victory status is pretty much done

--- src/main/java/it/escape/client/model/VictoryStatus.java
+++ src/main/java/it/escape/client/model/VictoryStatus.java
@@ -1,6 +1,10 @@
 package it.escape.client.model;
 
+import it.escape.server.model.game.players.PlayerTeams;
+import it.escape.strings.StringRes;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class VictoryStatus {
@@ -18,6 +22,43 @@ public class VictoryStatus {
 		alienWinners = new ArrayList<String>();
 		aliensDefeated = false;
 		humansDefeated = false;
+	}
+	
+	public void setTeamDefeated(String name) {
+		if (name.equals(PlayerTeams.HUMANS.toString())) {
+			humansDefeated = true;
+		} else if (name.equals(PlayerTeams.ALIENS.toString())) {
+			aliensDefeated = true;
+		}
+	}
+	
+	public void addWinners(String team, String list) {
+		if (team.equals(PlayerTeams.HUMANS.toString())) {
+			addTeamWinners(list, humanWinners);
+		} else if (team.equals(PlayerTeams.ALIENS.toString())) {
+			addTeamWinners(list, alienWinners);
+		}
+	}
+	
+	private void addTeamWinners(String list, List<String> dest) {
+		String parts[] = list.split(StringRes.getString("messaging.playerListSeparator"));
+		dest.addAll(Arrays.asList(parts));
+	}
+
+	public List<String> getHumanWinners() {
+		return humanWinners;
+	}
+
+	public List<String> getAlienWinners() {
+		return alienWinners;
+	}
+
+	public boolean isAliensDefeated() {
+		return aliensDefeated;
+	}
+
+	public boolean isHumansDefeated() {
+		return humansDefeated;
 	}
 	
 	
