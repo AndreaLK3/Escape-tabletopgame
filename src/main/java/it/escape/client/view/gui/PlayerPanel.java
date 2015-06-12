85ba1c6a858cef1a1ea6f85663626bf63d943ac4
Andrea Lekkas <andrea.lekkas@gmail.com>
2015-06-12 20:37:58 +0200

    fixed unchecked expansion for the playerPanels in DumbSwingView

--- src/main/java/it/escape/client/view/gui/PlayerPanel.java
+++ src/main/java/it/escape/client/view/gui/PlayerPanel.java
@@ -1,37 +1,51 @@
 package it.escape.client.view.gui;
 
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class PlayerPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
 	private GridBagConstraints constraints = new GridBagConstraints();
-	private JTextArea playerArea;
-	private JTextArea statusArea;
-	private JTextArea lastKnownActionArea;
+	private JTextField playerArea;
+	private JTextField statusArea;
+	private JTextField lastKnownActionArea;
 	
 	public PlayerPanel () {
-		this.setLayout(new GridLayout());
+		this.setLayout(new GridBagLayout());
 	
-		playerArea = new JTextArea();
+		playerArea = new JTextField();
 		playerArea.setEditable(false);
 		playerArea.setText("Unknown");
-		this.add(playerArea);
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		constraints.weightx = 0.3;
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		this.add(playerArea, constraints);
 		
-		statusArea = new JTextArea();
+		statusArea = new JTextField();
 		statusArea.setEditable(false);
 		statusArea.setText("Unknown");
-		this.add(statusArea);
+		constraints.gridx = 1;
+		constraints.gridy = 0;
+		constraints.weightx = 0.3;
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		this.add(statusArea, constraints);
 		
-		lastKnownActionArea = new JTextArea();
+		lastKnownActionArea = new JTextField();
 		lastKnownActionArea.setEditable(false);
 		lastKnownActionArea.setText("Unknown");
-		this.add(lastKnownActionArea);
+		constraints.gridx = 2;
+		constraints.gridy = 0;
+		constraints.weightx = 0.3;
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		this.add(lastKnownActionArea, constraints);
 	}
 	
 	
