c8e788643f0f8716ba0f22e35e8ae6ce6a09d4d8
Andrea Lekkas <andrea.lekkas@gmail.com>
2015-06-24 11:51:45 +0200

    updated ObjectCardActions sequence diagram + reorganized folders for the diagrams

--- src/main/java/it/escape/core/server/controller/game/actions/objectcardactions/Teleport.java
+++ src/main/java/it/escape/core/server/controller/game/actions/objectcardactions/Teleport.java
@@ -4,19 +4,17 @@ import it.escape.core.server.controller.UserMessagesReporter;
 import it.escape.core.server.controller.game.actions.HumanActionInterface;
 import it.escape.core.server.controller.game.actions.MapActionInterface;
 import it.escape.core.server.controller.game.actions.ObjectCardAction;
-import it.escape.core.server.controller.game.actions.PlayerActionInterface;
 import it.escape.core.server.model.game.gamemap.positioning.PositionCubic;
 
 public class Teleport implements ObjectCardAction {
 
 	public void execute(HumanActionInterface currentHuman, MapActionInterface map) {
-		PlayerActionInterface currentPlayer = (PlayerActionInterface) currentHuman;
 		PositionCubic c = map.getStartHumans();
-		map.updatePlayerPosition(currentPlayer, c);
+		map.updatePlayerPosition(currentHuman, c);
 		
 		// say where I am after the move
-		UserMessagesReporter.getReporterInstance(currentPlayer).reportMyUserPosition(
-				map.getPlayerAlphaNumPosition(currentPlayer));
+		UserMessagesReporter.getReporterInstance(currentHuman).reportMyUserPosition(
+				map.getPlayerAlphaNumPosition(currentHuman));
 	}
 
 }
