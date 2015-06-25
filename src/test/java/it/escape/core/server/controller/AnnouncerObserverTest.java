409572c95b9f708a3a1b18c2d48dd62d20a74796
Michele <michele.mm1234@gmail.com>
2015-06-25 19:34:19 +0200

    fixed sonar / jenkins problem

--- src/test/java/it/escape/core/server/controller/AnnouncerObserverTest.java
+++ src/test/java/it/escape/core/server/controller/AnnouncerObserverTest.java
@@ -2,23 +2,33 @@ package it.escape.core.server.controller;
 
 import static org.junit.Assert.assertTrue;
 import it.escape.core.server.model.AnnouncerStrings;
+import it.escape.tools.strings.FormatToPattern;
+import it.escape.tools.strings.StringRes;
 
 import java.util.Observable;
 import java.util.Observer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.Test;
 
 public class AnnouncerObserverTest implements Observer {
 	
 	private String last_message;
+	
+	private Pattern filter;
 
 	public AnnouncerObserverTest() {
+		filter = new FormatToPattern(StringRes.getString("messaging.playerConnected")).convert();
 	}
 
 	public void update(Observable o, Object arg) {
 		if (o instanceof AnnouncerStrings) {
 			AnnouncerStrings a = (AnnouncerStrings) o;
-			last_message = a.getMessage();
+			Matcher check = filter.matcher(a.getMessage());
+			if (check.matches()) {
+				last_message = a.getMessage();
+			}
 		}
 	}
 
